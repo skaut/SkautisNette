1d80dd22bb31dca9fafda874830775aabd0fc519
Jan Staněk <jan@jstanek.cz>
2022-04-10 18:52:36 +0200

    variables renamed

--- src/Tracy/EventDispatcher.php
+++ src/Tracy/EventDispatcher.php
@@ -26,25 +26,25 @@ class EventDispatcher implements EventDispatcherInterface
      */
     public function dispatch($event): void
     {
-        $id = new stdClass();
-        $id->fname = $event->getFname();
-        $id->args = $event->getArgs();
-        $hash = md5(serialize($id));
+        $requestId = new stdClass();
+        $requestId->fname = $event->getFname();
+        $requestId->args = $event->getArgs();
+        $requestHash = md5(serialize($requestId));
 
         switch (true) {
             case $event instanceof RequestPreEvent:
-                $this->requests[$hash] = SkautisQuery::createFromPreEvent($event);
+                $this->requests[$requestHash] = SkautisQuery::createFromPreEvent($event);
                 break;
             case $event instanceof RequestPostEvent:
-                if (key_exists($hash, $this->requests)) {
-                    $this->queries[] = SkautisQuery::updateFromPostEvent($this->requests[$hash], $event);
+                if (key_exists($requestHash, $this->requests)) {
+                    $this->queries[] = SkautisQuery::updateFromPostEvent($this->requests[$requestHash], $event);
                 } else {
                     $this->queries[] = SkautisQuery::createFromPostEvent($event); // should not happen
                 }
                 break;
             case $event instanceof RequestFailEvent:
-                if (key_exists($hash, $this->requests)) {
-                    $this->queries[] = SkautisQuery::updateFromFailEvent($this->requests[$hash], $event);
+                if (key_exists($requestHash, $this->requests)) {
+                    $this->queries[] = SkautisQuery::updateFromFailEvent($this->requests[$requestHash], $event);
                 } else {
                     $this->queries[] = SkautisQuery::createFromFailEvent($event); // should not happen
                 }
